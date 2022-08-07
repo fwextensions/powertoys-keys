c3b0ee0ac97b19b1803abc75919ce91d145edfb1
John Dunning <fw@johndunning.com>
2022-08-06 19:13:38 -0700

    Add function to create YAML settings file if it doesn't exist

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import fs from "node:fs/promises";
+import fsp from "node:fs/promises";
+import fs from "node:fs";
 import { resolve } from "node:path";
 import { env } from "node:process";
 import yaml from "yaml";
@@ -6,8 +7,21 @@ import { convertToJSON, convertToYAML } from "./settings.js";
 
 const SettingsDir = "Microsoft/PowerToys/Keyboard Manager";
 const SettingsFilename = "default.json";
+const SettingsPath = resolve(env.LOCALAPPDATA, SettingsDir, SettingsFilename);
 const BackupFilename = "backup default.json";
 const YamlFilename = "Keyboard Manager Settings.yaml";
+const ApplyChangesMessage = 'Now open PowerToys, click "Keyboard Manager", click "Remap a Shortcut", and then click "OK" to apply these changes.';
+
+async function writeYamlSettings()
+{
+	if (!fs.existsSync(YamlFilename)) {
+		const json = await fsp.readFile(SettingsPath, "utf8");
+		const kmSettings = JSON.parse(json);
+		const yamlSettings = convertToYAML(kmSettings);
+
+		await fsp.writeFile(YamlFilename, yamlSettings);
+	}
+}
 
 async function writeKMSettings(
 	settings)
@@ -17,19 +31,14 @@ async function writeKMSettings(
 	const settingsPath = resolve(settingsDir, SettingsFilename);
 	const backupPath = resolve(settingsDir, BackupFilename);
 
-	await fs.copyFile(settingsPath, backupPath);
-	await fs.writeFile(settingsPath, json);
+	await fsp.copyFile(settingsPath, backupPath);
+	await fsp.writeFile(settingsPath, json);
+	console.log(ApplyChangesMessage);
 }
 
-//const json = await fs.readFile(resolve(env.LOCALAPPDATA, SettingsDir, SettingsFilename), "utf8");
-//const kmbSettings = JSON.parse(json);
-//const yamlSettings = convertToYAML(kmbSettings);
-//const jsonSettings = convertToJSON(yaml.parse(yamlSettings));
+await writeYamlSettings();
 
-//await fs.writeFile(YamlFilename, yamlSettings);
-
-const yamlSettings = await fs.readFile(YamlFilename, "utf8");
+const yamlSettings = await fsp.readFile(YamlFilename, "utf8");
 const settings = yaml.parse(yamlSettings);
 
 await writeKMSettings(settings);
-console.log('Now open PowerToys, click "Keyboard Manager", click "Remap a Shortcut", and then click "OK" to apply these changes.');
